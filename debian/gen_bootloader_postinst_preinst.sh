dffeb0c154ae1f549db8ceaf67e302774e09d302
Alex Bradbury <asb@asbradbury.org>
2015-01-29 21:01:31 +0000

    fix .install for /boot and be sure to include dtb overlays

---
+++ debian/gen_bootloader_postinst_preinst.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if ! [ -d ../boot ]; then
+  printf "Can't find boot dir. Run from debian subdir\n"
+  exit 1
+fi
+
+printf "#!/bin/sh\n" > raspberrypi-bootloader.postinst
+for FN in ../boot/* ../boot/*/*; do
+  FN=${FN#../boot/}
+  if ! [ -d "$FN" ]; then
+    printf "rm -f /boot/$FN\n" >> raspberrypi-bootloader.postinst
+    printf "dpkg-divert --package rpikernelhack --remove --rename /boot/$FN\n" >> raspberrypi-bootloader.postinst
+    printf "sync\n" >> raspberrypi-bootloader.postinst
+  fi
+done
+printf "#DEBHELPER#\n" >> raspberrypi-bootloader.postinst
+
+printf "#!/bin/sh\n" > raspberrypi-bootloader.preinst
+printf "mkdir -p /usr/share/rpikernelhack/overlays" >> raspberrypi-bootloader.preinst
+for FN in ../boot/* ../boot/*/*; do
+  FN=${FN#../boot/}
+  if ! [ -d "$FN" ]; then
+    printf "dpkg-divert --package rpikernelhack --divert /usr/share/rpikernelhack/$FN /boot/$FN\n" >> raspberrypi-bootloader.preinst
+  fi
+done
+printf "#DEBHELPER#\n" >> raspberrypi-bootloader.preinst
