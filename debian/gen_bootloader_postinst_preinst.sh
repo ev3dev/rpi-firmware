9be421352b3a1cbd0d1942b1a6d70f42a67b032e
Alex Bradbury <asb@asbradbury.org>
2015-01-29 21:57:26 +0000

    fix install config

--- debian/gen_bootloader_postinst_preinst.sh
+++ debian/gen_bootloader_postinst_preinst.sh
@@ -7,8 +7,8 @@ fi
 
 printf "#!/bin/sh\n" > raspberrypi-bootloader.postinst
 for FN in ../boot/* ../boot/*/*; do
-  FN=${FN#../boot/}
   if ! [ -d "$FN" ]; then
+    FN=${FN#../boot/}
     printf "rm -f /boot/$FN\n" >> raspberrypi-bootloader.postinst
     printf "dpkg-divert --package rpikernelhack --remove --rename /boot/$FN\n" >> raspberrypi-bootloader.postinst
     printf "sync\n" >> raspberrypi-bootloader.postinst
@@ -17,10 +17,10 @@ done
 printf "#DEBHELPER#\n" >> raspberrypi-bootloader.postinst
 
 printf "#!/bin/sh\n" > raspberrypi-bootloader.preinst
-printf "mkdir -p /usr/share/rpikernelhack/overlays" >> raspberrypi-bootloader.preinst
+printf "mkdir -p /usr/share/rpikernelhack/overlays\n" >> raspberrypi-bootloader.preinst
 for FN in ../boot/* ../boot/*/*; do
-  FN=${FN#../boot/}
   if ! [ -d "$FN" ]; then
+    FN=${FN#../boot/}
     printf "dpkg-divert --package rpikernelhack --divert /usr/share/rpikernelhack/$FN /boot/$FN\n" >> raspberrypi-bootloader.preinst
   fi
 done
